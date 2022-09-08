3476ab81bdc89521f50c2e335b27664a98b2cf70
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2022-09-08 14:54:00 -0700

    Commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 /**
+ *main - entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (success)
+ * /
 int main(void)
-
 {
-
-	 puts("\"Programming is like building a multilingual puzzle");
-
-	  return (0);
-
+     	 puts("\"Programming is like building a multilingual puzzle\n");
+	 return(0);
 }
