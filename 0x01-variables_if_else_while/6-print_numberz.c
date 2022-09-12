4abad745a19efbbcd8ffa9d01bd0aa33088f7a6d
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2022-09-12 13:14:53 -0700

    Commit message

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative 
+ * main - print if the number is postive, zero, or negative
  *
- * Description: using the main function 
+ * Description: using the main function
  * this program prints "Programming is positive, zero, or negative"
- *  Return: 0 
+ *  Return: 0
  */
 int main(void)
 {
-int n; 
-for (n = '0'; n <= '9'; n++) 
+int n;
+for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
 }
-putchar('\n'); 
+putchar('\n');
 return (0);
 }
