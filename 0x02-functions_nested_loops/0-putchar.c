ddee779fd497be871fd7bcc7545f89010129c720
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2022-09-13 10:18:19 -0700

    Commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  main - print putchar
+ * main - Entry point
  *
- *  Return : 0
+ * Return: 0
  */
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
