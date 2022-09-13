039c7101a5b48adf9e8c90f5e0310cbd4e7631e6
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2022-09-13 08:45:52 -0700

    Commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include <main.h>
 /**
- * main - Entry point
+ * main - print putchar
  *
  * Return : 0
  */
 int main(void)
 {
-	char *sh = "_putchar";
+	_putchar( '_' );
+	_putchar( 'p' );
+	_putchar( 'u' );
+	_putchar( 't' );
+	_putchar( 'c' );
+	_putchar( 'h' );
+	_putchar( 'a' );
+	_putchar( 'r' );
+	_putchar( '\n' );
 
-	while(*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
 	return (0);
 }
-	
